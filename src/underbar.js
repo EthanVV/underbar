d7b7c11e25f96b9f58f047c51f05294e6e2d727f
EthanVV <ethan.vanvleck@gmail.com>
2018-08-28 15:49:41 -0700

    Comment added to test git workflow

--- src/underbar.js
+++ src/underbar.js
@@ -3,6 +3,8 @@
 
   window._ = {};
 
+  // This comment is to prove I can commit changes.
+
   // Returns whatever value is passed as the argument. This function doesn't
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.

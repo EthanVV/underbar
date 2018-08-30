25a467b512b74bcff3cd5feb9b763ffcd1772b4c
EthanVV <ethan.vanvleck@gmail.com>
2018-08-30 07:29:53 -0700

    Implemented _.extend function

--- src/underbar.js
+++ src/underbar.js
@@ -127,26 +127,14 @@
     });
   };
 
-
-  /**
-   * OBJECTS
-   * =======
-   *
-   * In this section, we'll look at a couple of helpers for merging objects.
-   */
-
-  // Extend a given object with all the properties of the passed in
-  // object(s).
-  //
-  // Example:
-  //   var obj1 = {key1: "something"};
-  //   _.extend(obj1, {
-  //     key2: "something new",
-  //     key3: "something else new"
-  //   }, {
-  //     bla: "even more stuff"
-  //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      var objFromArgs = arguments[i];
+      _.each(objFromArgs, function(item, key) {
+       obj[key] = item;
+      });
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
